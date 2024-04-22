20f2f2f23f37900d0f0536dd0f804219edd62a94
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-22 15:43:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let inputField = documet.querySelector("#fname")
+
+inputField.addEventListener("blur", () =>{
+	let val = inputField.value;
+	val = val.toUpperCase();
+	inputField.value = val;
+})
